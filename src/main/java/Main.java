2625416a23c6de510e484a48ee3d17566f01d7b4
m.kosek <m.kosek@netsystem.cz>
2024-07-22 21:48:50 +0200

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -24,18 +25,35 @@ public class Main {
       System.out.println(line);
       
       String[] HttpRequest = line.split(" ", 0);
-      
+
+      OutputStream output = clientSocket.getOutputStream();
+      String[] str = HttpRequest[1].split("/");
+
       if (HttpRequest[1].equals("/")) {
-        clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        System.out.println("version");
+        String response = "HTTP/1.1 200 OK\r\n"
+                          + "Content-Type: text/plain\r\n"
+                          + "Content-Length: 0\r\n\r\n";
+        output.write(response.getBytes());
       }
-      else if (HttpRequest[1].startsWith("/echo/")) {
-        String msg = HttpRequest[1].substring(6);
-        String header = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", msg.length(), msg);
-        clientSocket.getOutputStream().write(header.getBytes());
+      else if (str[1].equals("user-agent")) {
+        reader.readLine();
+        String useragent = reader.readLine().split("\\s+")[1];
+        String reply = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s\r\n", useragent.length(), useragent);
+        output.write(reply.getBytes());
+      }
+      else if ((str.length > 2 && str[1].equals("echo"))) {
+        String responsebody = str[2];
+        String finalstr = "HTTP/1.1 200 OK\r\n"
+                          + "Content-Type: text/plain\r\n"
+                          + "Content-Length: " + responsebody.length() +
+                          "\r\n\r\n" + responsebody;
+        output.write(finalstr.getBytes());
       }
       else {
-        clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
       }
+      output.flush();
 
       serverSocket.close();
     } catch (IOException e) {
